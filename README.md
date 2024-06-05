b0f9c355ef2008dfa903dc9bc39aba495129e95a
Julian <me@julianpryce.com>
2024-06-05 13:20:18 -0400

    Updated readme, added Highlight runtime status check

---
+++ README.md
@@ -0,0 +1,11 @@
+# Highlight-Demo-App
+
+This is a demo of the Highlight Runtime API. It's a simple app that shows you how to use the API to get an access token to authenticate your Highlight users on the backend.
+
+## Development
+
+To get started, run `pnpm dev`. This will start the Next.js app.
+
+## Deploying
+
+We recommend deploying through [Vercel](https://vercel.com).
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@
 import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [runningInHighlight, setRunningInHighlight] = useState<boolean | null>(
+    null
+  );
   const [token, setToken] = useState("");
 
   useEffect(() => {
@@ -10,7 +13,15 @@ export default function Home() {
 
     async function fetchToken() {
       // Dynamic import to avoid issues with Next prerendering
-      const Highlight = (await import("@highlight-ai/app-runtime")).default;
+
+      let Highlight;
+      try {
+        Highlight = (await import("@highlight-ai/app-runtime")).default;
+        setRunningInHighlight(true);
+      } catch (error) {
+        setRunningInHighlight(false);
+        return;
+      }
 
       const { accessToken, refreshToken } = await Highlight.auth.signIn();
 
@@ -21,8 +32,13 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="text-white p-4">
-      <h1>Highlight Demo App</h1>
+    <main className="p-4 space-y-2">
+      <h1 className="text-2xl font-bold">Highlight Demo App</h1>
+      <p>This is a demo app to showcase the Highlight Runtime API.</p>
+      {runningInHighlight === null && <p>Loading...</p>}
+      {runningInHighlight === false && (
+        <p className="text-red-500">Not running in Highlight</p>
+      )}
       {token && <p>Your token is {token}</p>}
     </main>
   );
