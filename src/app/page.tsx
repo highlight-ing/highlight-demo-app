65e61a08f46ceeebd4ad0790fc8827e71f941ebd
Julian <me@julianpryce.com>
2024-06-05 16:09:17 -0400

    Updated to use newer Highlight API version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Highlight from "@highlight-ai/app-runtime";
 
 export default function Home() {
   const [runningInHighlight, setRunningInHighlight] = useState<boolean | null>(
@@ -12,27 +13,19 @@ export default function Home() {
     // On page load, fetch a new access token
 
     async function fetchToken() {
-      // Dynamic import to avoid issues with Next prerendering
-
-      let Highlight;
       try {
-        Highlight = (await import("@highlight-ai/app-runtime")).default;
-        setRunningInHighlight(true);
+        const { accessToken, refreshToken } = await Highlight.auth.signIn();
+        setToken(accessToken);
       } catch (error) {
         setRunningInHighlight(false);
-        return;
       }
-
-      const { accessToken, refreshToken } = await Highlight.auth.signIn();
-
-      setToken(accessToken);
     }
 
     fetchToken();
   }, []);
 
   return (
-    <main className="p-4 space-y-2">
+    <main className="p-4 space-y-2 ">
       <h1 className="text-2xl font-bold">Highlight Demo App</h1>
       <p>This is a demo app to showcase the Highlight Runtime API.</p>
       {runningInHighlight === null && <p>Loading...</p>}
